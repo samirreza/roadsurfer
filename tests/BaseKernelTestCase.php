4546940087a10bcb92062296ab8b96cce11767b2
Amirreza Yeganegi <amirreza.developer@gmail.com>
2021-10-06 14:28:18 +0330

    Write BaseKernelTestCase class

---
+++ tests/BaseKernelTestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests;
+
+use Zenstruck\Foundry\Test\Factories;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+abstract class BaseKernelTestCase extends KernelTestCase
+{
+    use Factories;
+}

a291192fac625c8c138bb10ef9ad0e6b22c0400f
Amirreza Yeganegi <amirreza.developer@gmail.com>
2021-10-06 14:29:09 +0330

    Fix bug in BaseKernelTestCase

--- tests/BaseKernelTestCase.php
+++ tests/BaseKernelTestCase.php
@@ -8,4 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 abstract class BaseKernelTestCase extends KernelTestCase
 {
     use Factories;
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+    }
 }
